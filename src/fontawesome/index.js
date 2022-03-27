05885ce076545826ca78745029b7673885e424d8
Soumitha Bhaskara <soumitha@revvsales.com>
2022-03-27 13:32:22 +0530

    fix: removed usless files

--- src/fontawesome/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DuoTone } from "./duoTone";
-export { default as Regular } from "./regular";
-export { default as Solid } from "./solid";
-export { default as Brands } from "./brands";
-export { default as Light } from "./light";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./fontawesome";
 
 ReactDOM.render(
   <Router>
