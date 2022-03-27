f1414d3ed59a5c42becd2be9372f7f13c3a94e10
Soumitha Bhaskara <soumitha@revvsales.com>
2022-03-27 14:01:34 +0530

    added files

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,5 +1,6 @@
 import { Switch, Route } from "react-router";
 import HOW from "./how";
+import LANDING from "./landing";
 import WHAT from "./what";
 import WHEN from "./when";
 import WHERE from "./where";
@@ -9,6 +10,7 @@ import WHY from "./why";
 export const Routes = () => {
   return (
     <Switch>
+      <Route path="/" exact component={LANDING} />
       <Route path="/who" component={WHO} />
       <Route path="/what" component={WHAT} />
       <Route path="/where" component={WHERE} />
--- src/components/who.jsx
+++ src/components/who.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+const data = [
+  "20-21",
+  "determination",
+  "Data Driven",
+  "Humble",
+  "Resilient",
+  "Patience",
+  "Curious",
+];
 
 export default function WHO() {
-  return <div>WHO</div>;
+  return (
+    <>
+      <div className="div-col">
+        <p className="tag-head">WHO</p>
+        <ul className="col-ul">
+          {data.map((item, index) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+        <Link to="/what" className="button-next">
+          WHAT
+        </Link>
+      </div>
+    </>
+  );
 }
